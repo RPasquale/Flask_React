2e83aa03847c75dd0d73aa920dc0b6752e98e338
Robbie Pasquale <rpas0004@student.monash.edu>
2023-06-20 18:32:35 +1000

    working initial

--- venv/app.py
+++ venv/app.py
@@ -1,17 +1,12 @@
 from flask import Flask, request, jsonify
-from sklearn.datasets import load_diabetes, fetch_california_housing
+from sklearn.datasets import  fetch_california_housing
 import numpy as np
 from sklearn.base import BaseEstimator
 from flask_cors import CORS
 app = Flask(__name__)
-CORS(app)
-#app.config['CORS_HEADERS'] = 'Content-Type'
+#CORS(app)
+CORS(app, origins='http://localhost:3000')
 
-# Define the route for getting the feature names
-@app.route('/feature_names', methods=['GET'])
-def get_feature_names():
-    feature_names = ["Feature 1", "Feature 2"]  # Replace with actual feature names
-    return jsonify(feature_names)
 
 class KnnRegressor(BaseEstimator):
     def __init__(self, k=5):
@@ -34,11 +29,6 @@ class KnnRegressor(BaseEstimator):
 
 
 # Load the trained models and other necessary variables
-diabetes = load_diabetes()
-X_diab, y_diab = diabetes.data, diabetes.target
-knn_reg_diab = KnnRegressor(k=10)
-knn_reg_diab.fit(X_diab, y_diab)
-
 california_housing = fetch_california_housing()
 X_cali, y_cali = california_housing.data, california_housing.target
 knn_reg_cali = KnnRegressor(k=10)
@@ -48,29 +38,29 @@ knn_reg_cali.fit(X_cali, y_cali)
 @app.route('/predict', methods=['POST'])
 def predict():
     user_input = request.json
+    
+    # Extract the features from the user input
+    feature_values = [
+        float(user_input['feature1']),
+        float(user_input['feature2']),
+        float(user_input['feature3']),
+        float(user_input['feature4']),
+        float(user_input['feature5']),
+        float(user_input['feature6']),
+        float(user_input['feature7']),
+        float(user_input['feature8']),
 
-    # Perform the prediction using the trained models and user input
-    user_input_diab = user_input.get('diabetes', None)
-    user_input_cali = user_input.get('california', None)
+    ]
+    
+    # Convert the feature values to a numpy array
+    input_data = np.array(feature_values).reshape(1, -1)
+    
+    # Make the prediction using the trained model
+    prediction = knn_reg_cali.predict(input_data)[0]
 
-    prediction_diab = None
-    prediction_cali = None
-
-    if user_input_diab is not None:
-        user_data_diab_arr = np.array(list(user_input_diab.values()))
-        prediction_diab = knn_reg_diab.predict([user_data_diab_arr])
-
-    if user_input_cali is not None:
-        user_data_cali_arr = np.array(list(user_input_cali.values()))
-        prediction_cali = knn_reg_cali.predict([user_data_cali_arr])
-
-    # Return the prediction results as JSON
-    results = {
-        'diabetes_prediction': prediction_diab.tolist(),
-        'california_prediction': prediction_cali.tolist()
-    }
-    return jsonify(results)
-
+    # Return the prediction result as JSON
+    result = {'prediction': prediction}
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run()
